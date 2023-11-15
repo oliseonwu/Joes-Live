e8cddd0c0a4354181cc6ae089105fd5dd88de6c2
Olisemedua Onwuatogwu <oonwuatogwu@csumb.edu>
2023-11-15 11:50:56 -0800

    Cloud now produces lightning
    The angry cloud would come in and bring out its lightning when he's on top of Joe head then it leaves and destroys itself.

--- Assets/angryCloud.cs
+++ Assets/angryCloud.cs
@@ -7,14 +7,18 @@ public class angryCloud : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody rb;
+    public GameObject electricity;
+    private Vector3 startPos; 
     [Range(1, 100)] public float moveSpeed;
-    private enum State{MOVE, IDEL};
+    private enum State{MOVE_LEFT, IDEL, MOVE_RIGHT};
 
-    private State cloudState = State.MOVE;
+    private State cloudState = State.MOVE_LEFT;
     public Animator animator;
     public static event Action shockAnimEvent;
     void Start()
     {
+        startPos = gameObject.transform.position;
+        Debug.Log(startPos);
         rb = GetComponent<Rigidbody>();
     }
     
@@ -27,15 +31,30 @@ public class angryCloud : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (cloudState == State.MOVE)
+        if (cloudState == State.MOVE_LEFT)
         {
             MoveLeft();
         }
+        if (cloudState == State.MOVE_RIGHT)
+        {
+            MoveRight();
+        }
+        
     }
 
     private void MoveLeft()
     {
         rb.AddForce(new Vector3(-1.0f * moveSpeed,0.0f, 0.0f) * Time.deltaTime,ForceMode.Force);
+    }
+    
+    private void MoveRight()
+    {
+        rb.AddForce(new Vector3(1.0f * moveSpeed,0.0f, 0.0f) * Time.deltaTime,ForceMode.Force);
+        if (startPos.x < gameObject.transform.position.x)
+        {
+            Destroy(gameObject);
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -47,7 +66,21 @@ public class angryCloud : MonoBehaviour
             
             animator.SetBool("Shock", true);
             shockAnimEvent?.Invoke();
+            electricity.gameObject.SetActive(true);
+            StartCoroutine(leave());
+
         }
+    }
+
+    IEnumerator leave()
+    {
+        yield return new WaitForSeconds(2);
+        electricity.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        cloudState = State.MOVE_RIGHT;
+        animator.SetBool("Shock", false);
+        animator.SetBool("smirk & look left", true);
+
     }
 }
 
