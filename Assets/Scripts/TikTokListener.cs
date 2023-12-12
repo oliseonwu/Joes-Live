ee63de27e6456c8f8c77319b41985426a588823d
Olisemedua Onwuatogwu <oonwuatogwu@csumb.edu>
2023-12-12 00:52:09 -0800

    Added Icons of Gifts my character supports
    I added Hat and mustache, Rose and Lightning Bolt. Joe only replies to only these animations for now.

--- Assets/Scripts/TikTokListener.cs
+++ Assets/Scripts/TikTokListener.cs
@@ -8,13 +8,21 @@ public class TikTokListener : MonoBehaviour
 {
     private WebSocket ws;
     public GiftBatchHandler giftBatchHandler;
+    private int reconnectAttempts = 0;
+    private const int MaxReconnectAttempts = 10;
+    private float reconnectDelay = 1f; // Delay in seconds
+    public UnityMainThreadDispatcher unityMainThreadDispatcher;
     void Start()
     {
+        ConnectToWebSocket();
+    }
+    
+    private void ConnectToWebSocket()
+    {
         ws = new WebSocket("ws://localhost:8080");
-
-        ws.OnMessage +=  OnMessageReceived;
+        ws.OnMessage += OnMessageReceived;
+        ws.OnClose += OnWebSocketClosed;
         ws.Connect();
-        
     }
 
     void OnMessageReceived(object Serverws,  MessageEventArgs e)
@@ -30,6 +38,27 @@ public class TikTokListener : MonoBehaviour
         
         
         // {"type":"gift","uniqueId":"1rabbitcatcher","giftName":"Rose"}
+    }
+
+    private void OnWebSocketClosed(object sender, CloseEventArgs e)
+    {
+        Debug.Log("Disconnected from socket server. Attempting to reconnect...");
+        if (reconnectAttempts < MaxReconnectAttempts)
+        {
+            UnityMainThreadDispatcher.Enqueue(()=> Invoke(nameof(Reconnect), reconnectDelay));
+            reconnectDelay *= 2; // Exponential backoff
+            reconnectAttempts++;
+        }
+        else
+        {
+            Debug.LogError("Maximum reconnect attempts reached. Stopping reconnection attempts.");
+        }
+    }
+
+    private void Reconnect()
+    {
+        Debug.Log("Reconnecting to server");
+        ConnectToWebSocket();
     }
 
     
