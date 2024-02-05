264bb9f02e2b8daea12eec690ce3d9e292d7a9c3
Olisemedua Onwuatogwu <oonwuatogwu@csumb.edu>
2024-02-05 14:08:34 -0800

    Finished working on the animation
    I moved joes bones to make him point to the bubble
    
    Now I need to make the logic to triggering that animation using the joes contex manager

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Utilities : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static bool showLogs = false;
+    public static bool showLogs = true;
     void Start()
     {
         
